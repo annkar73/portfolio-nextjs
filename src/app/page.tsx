457d575b6a3e0ab09a96b20b7aaf2cf4ab81f057
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-05-29 01:08:50 +0200

    border test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,16 @@ export default function Home() {
       <Hero />
 
       <main className="p-6 text-[var(--color-foreground)] bg-white max-w-3xl mx-auto">
-        <div className="w-full max-w-3xl border border-[var(--color-secondary-accent)] rounded-lg p-6 mb-8">
+        <div className="w-full max-w-3xl border-0 border-l-5 border-[var(--color-accent)] rounded-none p-6 mb-8">
           kortfattad info om utbildningen
           </div>
-                  <div className="w-full max-w-3xl border border-[var(--color-secondary-accent)] rounded-lg p-6 mb-8">
+                  <div className="w-full max-w-3xl border-0 border-l-5 border-[var(--color-accent)] rounded-none p-6 mb-8">
           kortfattad info om CV
           </div>
-                  <div className="w-full max-w-3xl border border-[var(--color-secondary-accent)] rounded-lg p-6 mb-8">
+                  <div className="w-full max-w-3xl border-0 border-l-5 border-[var(--color-accent)] rounded-none p-6 mb-8">
           karusell med projektkort
           </div>
-                  <div className="w-full max-w-3xl border border-[var(--color-secondary-accent)] rounded-lg p-6 mb-8">
+                  <div className="w-full max-w-3xl border-0 border-l-5 border-[var(--color-accent)] rounded-none p-6 mb-8">
           kortfattad kontaktinfo
           </div>
 
