426614f51e505709d5903bdc37a2f9b6648c0758
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-05-29 14:15:21 +0200

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
+import nextI18NextConfig from "@/next-18next.config";
 
 const nextConfig: NextConfig = {
+  ...nextI18NextConfig,
   /* config options here */
 };
 
---
+++ src/next-18next.config.ts
@@ -0,0 +1,11 @@
+// next-i18next.config.ts
+import type { UserConfig } from 'next-i18next';
+
+const nextI18NextConfig: UserConfig = {
+  i18n: {
+    defaultLocale: 'sv',
+    locales: ['sv', 'en'],
+  },
+};
+
+export default nextI18NextConfig;
