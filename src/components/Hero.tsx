4ea16fb9a357b8a5dc9b100a79f2775169a874db
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-05-29 15:02:25 +0200

    update hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 export default function Hero() {
+  const t = useTranslations("hero");
+
   return (
     <section className="relative h-[60vh] w-full">
       <Image
@@ -11,19 +16,17 @@ export default function Hero() {
         className="object-cover border-none"
       />
       <div className="absolute inset-0 bg-black/50 flex flex-col items-center justify-center space-y-4 py-8">
-        <h1 className="text-(--color-accent) text-4xl md:text-5xl font-bold text-center">
-          Anna Karlsen
+        <h1 className="text-[var(--color-accent)] text-4xl md:text-5xl font-bold text-center">
+          {t("name")}
         </h1>
-        <h3 className="text-(--color-background) text-xl md:text-3xl italic font-mono">&lt; Front End Developer /&gt;</h3>
-        <div className=" my-10 mx-10 text-(--color-accent) text-xl md:text-2xl space-y-4">
+        <h3 className="text-[var(--color-background)] text-xl md:text-3xl italic font-mono">
+          {t("title")}
+        </h3>
+        <div className="my-10 mx-10 text-[var(--color-accent)] text-xl md:text-2xl space-y-4">
           <p>
-            <q className="italic">När du väl slutar lära dig börjar du dö</q>, sa Albert Einstein.
-            </p>
-            <p>
-                Riktigt så illa är det väl inte? Jag tror däremot att det är viktigt att ständigt
-            utvecklas och lära sig nya saker.
-            </p>
-          
+            <q className="italic">{t("quote")}</q>
+          </p>
+          <p>{t("text1")}</p>
         </div>
       </div>
     </section>
