359864d50fd89fc4a4b775d17a7da792c5d35978
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-06-06 17:25:37 +0200

    update contact page

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -11,19 +11,21 @@ export default function ContactPage() {
       <h1 className="text-3xl font-bold mb-6 text-center">{t("contact.title")}</h1>
       <p className="max-w-3xl text-lg mb-8 text-center">{t("contact.intro")}</p>
       <ContactForm />
-      <div className="w-full bg-[var(--color-foreground)] p-6 mt-8 mx-auto">
-      <h2 className="text-2xl font-semibold mt-5 mb-4 text-center text-[var(--color-accent)]">{t("contact.subtitle")}</h2>
+
+      
+      <div className="min-w-full bg-[var(--color-foreground)] p-6 mt-8 mb-0 -mx-4">
+      <h2 className="text-2xl font-bold mt-5 mb-4 text-center text-[var(--color-accent)]">{t("contact.subtitle")}</h2>
 
       <p className="text-center mb-4 text-[var(--color-background)]">
-        {t("contact.addressText")}: <a href={`mailto:${t("contact.address")}`} className="text-[var(--color-accent)] hover:underline">{t("contact.address")}</a>
+        {t("contact.addressText")}: <a href={`mailto:${t("contact.address")}`} className="text-[var(--color-accent)] font-semibold hover:underline">{t("contact.address")}</a>
       </p>
 
       <p className="text-center mb-4 text-[var(--color-background)]">
-        {t("contact.githubText")} <a href={t("contact.github")} target="_blank" rel="noopener noreferrer" className="text-[var(--color-accent)] hover:underline">GitHub</a>   
+        {t("contact.githubText")} <a href={t("contact.github")} target="_blank" rel="noopener noreferrer" className="text-[var(--color-accent)] font-semibold hover:underline">GitHub </a>   
       </p>
 
       <p className="text-center mb-4 text-[var(--color-background)]">
-        {t("contact.linkedinText")} <a href={t("contact.linkedin")} target="_blank" rel="noopener noreferrer" className="text-[var(--color-accent)] hover:underline">LinkedIn</a> 
+        {t("contact.linkedinText")} <a href={t("contact.linkedin")} target="_blank" rel="noopener noreferrer" className="text-[var(--color-accent)] font-semibold hover:underline">LinkedIn</a> 
       </p>
       </div>
     </main>
